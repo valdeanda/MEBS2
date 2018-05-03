268f33928565fdfd57d37ec3432a18946b4e3389
valdeanda <valdeanda@ciencias.unam.mx>
2018-05-03 19:22:13 +0200

    not fixed yet

--- mebsv2.pl
+++ mebsv2.pl
@@ -193,7 +193,6 @@ foreach $c (0 .. $#cycles)
     close(COMPFILE);
      
      $pathways_header ="\t$cycles[$c]_<comp>";
-   # $pathways_header = "\t<cycle_comp>";
     foreach $pw (sort {$a<=>$b} keys(%{$pathways{$cycles[$c]}}))
     {
       $pathways_header .= "\t$cycles[$c]_$pw";
@@ -211,6 +210,7 @@ foreach $f (0 .. $#valid_infiles)
   foreach $c (0 .. $#cycles)
   {
     $path = $paths[$c];
+    print $path; 
     $cycle = $cycles[$c];
     $comp = $completeness[$c];
     $score = 'NA';
